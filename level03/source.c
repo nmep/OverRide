20209c60b79865987107ad271ac7fb66cf369fe8
user <lyess15@sfr.fr>
2026-07-01 01:26:13 +0200

    dans la fonction test il add une adresse a eax +37 puis rentre dedans donc en fonction de la valeur qui est par defaut a 259 dans la fonction main, on peut le voir avec gdb chose a faire, continuer de traduire

---
+++ level03/source.c
@@ -0,0 +1,26 @@
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+void test(int variable, int 0x1337d00d)
+{
+    if ((0x1337d00d - variable) > 0x15)
+    {
+        // main + 259
+    }
+    
+
+}
+
+int main()
+{
+    srand(time(0));
+    puts("***********************************");
+    puts("*\t\tlevel03\t\t**");
+    puts("***********************************");
+    puts("Password:");
+    scanf("%d", variable);
+    tesst(variable, 0x1337d00d);
+
+
+}
